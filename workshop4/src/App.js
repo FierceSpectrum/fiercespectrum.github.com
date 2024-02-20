cc6f5975e33d82a7117a550c8c8257d7b5d72e5a
FierceSpectrum <benjaminsandi@hotmail.com>
2024-02-20 11:43:16 -0600

    prueba react
    prueba react

---
+++ workshop4/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import UserName from './Patrones/Singleton/UserName';
+import UserAge from './Patrones/Singleton/UserAge';
+import Archivo from './Patrones/Decorator/Archivo';
+import ArchivoSin from './Patrones/Decorator/ArchivoSin';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={
+              <>
+              </>
+            } />
+            <Route path="/Singleton" element={
+              <>
+                <UserName />
+                <UserAge />
+              </>
+            } />
+            <Route path="/Decorator" element={
+              <>
+                <Archivo/>
+                <ArchivoSin/>
+              </>
+            } />
+
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
