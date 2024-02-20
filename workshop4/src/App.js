499ddbe88a882a616029e3017ad0ac4b5cdab539
FierceSpectrum <benjaminsandi@hotmail.com>
2024-02-20 15:42:47 -0600

    ya
    asdf

--- workshop4/src/App.js
+++ workshop4/src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={
               <>
+                <div>{'hola listo?'}</div>
               </>
             } />
             <Route path="/Singleton" element={
